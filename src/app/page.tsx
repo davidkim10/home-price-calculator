bec17c4632342c50bae6a770f5862cd085bef05d
David Kim <davidykim10@gmail.com>
2025-05-12 00:22:36 -0400

    fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Calculator } from "@/app/components/Calculator";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ]">
+    <div className="grid items-center justify-items-center min-h-screen p-4 gap-2]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Calculator />
       </main>
